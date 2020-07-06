4858843063be2336d898420e8c494f7795f80751
zzl-97 <1002196835@qq.com>
2020-07-06 10:14:13 +0800

    kotlin

---
+++ src/main/kotlin/com/huayei/Test01Application.kt
@@ -0,0 +1,13 @@
+package com.huayei
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Test01Application
+
+fun main(){
+    runApplication<Test01Application>()
+}
+
+
