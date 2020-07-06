010b8c046ffd7c9d60e93a139056c1b3c4c7271a
zzl-97 <1002196835@qq.com>
2020-07-06 09:37:40 +0800

    框架搭建

---
+++ src/main/java/com/huayei/Boottest.java
@@ -0,0 +1,16 @@
+package com.huayei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boottest {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(Boottest.class, args);
+        }
+
+
+
+}
