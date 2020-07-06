f13c0e51392b2a4c27b31f395b809176c4686d5f
zzl-97 <1002196835@qq.com>
2020-07-06 10:09:16 +0800

    kotlin

---
+++ src/main/kotlin/com/huayei/SpringBoot.kt
@@ -0,0 +1,12 @@
+package com.huayei
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBoot {
+
+    fun mian(){
+        SpringApplication.run(Boottest::class.java)
+    }
+}
