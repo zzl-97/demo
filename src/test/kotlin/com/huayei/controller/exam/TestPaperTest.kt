1aa98ee939ec0330af1737a698f70ee3cbbad3fa
1250812267 <1250812267@qq.com>
2020-07-14 15:52:09 +0800

    刘恒 建立测试类

---
+++ src/test/kotlin/com/huayei/controller/exam/TestPaperTest.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 福建华业信息技术有限公司.
+ */
+
+package com.huayei.controller.exam
+
+/**
+ * @Description TODO
+ * @Author  liuh
+ * @Email liuh@huayei.com
+ * @Date 2020/7/14 15:50
+ * @Since 1.0
+ **/
+class TestPaperTest {
+}
